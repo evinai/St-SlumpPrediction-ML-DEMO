8f59a853cec4adcf380a21e1e6badf2571818861
Togay Tunca <tuncatogay@gmail.com>
2021-05-17 03:19:51 +0300

    logoupdate

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ desc_temp = """
 
 			"""
 from PIL import Image 
-img = Image.open("togaylogogri.jpg")
+img = Image.open("togaylogogri.png")
 st.sidebar.image(img,width=270, caption='Demo-Lab')# 
 # st.sidebar.image('st-sidebar.jpg', width=200, align=Left)
 
